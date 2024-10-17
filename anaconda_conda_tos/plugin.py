189a709d5d0cb46be6a95fdd8078318fd2315f96
Ken Odegard <kodegard@anaconda.com>
2024-10-17 13:40:06 -0500

    Add linters (#24)

--- anaconda_conda_tos/plugin.py
+++ anaconda_conda_tos/plugin.py
@@ -1,30 +1,33 @@
 # Copyright (C) 2024 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-"""
-ToS subcommand and settings plugins.
-"""
+"""Conda ToS subcommand and settings plugins."""
 
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from conda.plugins import hookimpl, CondaSubcommand, CondaSetting
 from conda.common.configuration import PrimitiveParameter
+from conda.plugins import CondaSetting, CondaSubcommand, hookimpl
 
 if TYPE_CHECKING:
-    from argparse import Namespace, ArgumentParser
+    from argparse import ArgumentParser, Namespace
+    from typing import Iterator
 
 
-def configure_parser(parser: ArgumentParser):
-    pass
+def configure_parser(parser: ArgumentParser) -> None:
+    """Configure the parser for the `tos` subcommand."""
+    parser = parser
 
 
 def execute(args: Namespace) -> int:
+    """Execute the `tos` subcommand."""
+    args = args
     return 0
 
 
 @hookimpl
-def conda_subcommands():
+def conda_subcommands() -> Iterator[CondaSubcommand]:
+    """Return a list of subcommands for the anaconda-conda-tos plugin."""
     yield CondaSubcommand(
         name="tos",
         action=execute,
@@ -34,7 +37,8 @@ def conda_subcommands():
 
 
 @hookimpl
-def conda_settings():
+def conda_settings() -> Iterator[CondaSetting]:
+    """Return a list of settings for the anaconda-conda-tos plugin."""
     yield CondaSetting(
         name="auto_accept_tos",
         description="Automatically accept Terms of Service (ToS) for all channels.",
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -2,12 +2,13 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from __future__ import annotations
 
-from conda.base.context import context
-from anaconda_conda_tos.plugin import conda_subcommands, conda_settings
 from typing import TYPE_CHECKING
 
+from conda.base.context import context
+
+from anaconda_conda_tos.plugin import conda_settings, conda_subcommands
+
 if TYPE_CHECKING:
-    from pytest import MonkeyPatch
     from conda.testing.fixtures import CondaCLIFixture
 
 
@@ -15,7 +16,7 @@ def test_subcommands_hook() -> None:
     subcommands = list(conda_subcommands())
     assert len(subcommands) == 1
 
-    subcommands[0].name == "tos"
+    assert subcommands[0].name == "tos"
 
     assert "tos" in context.plugin_manager.get_subcommands()
 
@@ -24,12 +25,12 @@ def test_settings_hook() -> None:
     settings = list(conda_settings())
     assert len(settings) == 1
 
-    settings[0].name == "auto_accept_tos"
+    assert settings[0].name == "auto_accept_tos"
 
 
 def test_subcommand_tos(conda_cli: CondaCLIFixture) -> None:
     conda_cli("tos")
 
 
-def test_setting_auto_accept_tos(monkeypatch: MonkeyPatch) -> None:
+def test_setting_auto_accept_tos() -> None:
     assert not context.plugins.auto_accept_tos
