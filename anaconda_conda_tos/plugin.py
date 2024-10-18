b1585b580acba2d564303c3b56927ba9ca75d119
Ken Odegard <kodegard@anaconda.com>
2024-10-17 22:33:34 -0500

    Remove

--- anaconda_conda_tos/plugin.py
+++ anaconda_conda_tos/plugin.py
@@ -4,16 +4,13 @@
 
 from __future__ import annotations
 
-from datetime import datetime
 from typing import TYPE_CHECKING
 
 from conda.base.context import context
 from conda.common.configuration import PrimitiveParameter
 from conda.plugins import CondaSetting, CondaSubcommand, hookimpl
-from rich.console import Console
-from rich.table import Table
 
-from .tos import accept_tos, get_tos, reject_tos, view_tos
+from .tos import accept_tos, view_tos
 
 if TYPE_CHECKING:
     from argparse import ArgumentParser, Namespace
@@ -27,53 +24,15 @@ def configure_parser(parser: ArgumentParser) -> None:
 
     mutex = parser.add_mutually_exclusive_group()
     mutex.add_argument("--accept", "--agree", "--yes", action="store_true")
-    mutex.add_argument(
-        "--reject", "--disagree", "--no", "--withdraw", action="store_true"
-    )
     mutex.add_argument("--view", "--show", action="store_true")
-
-
-def accepted_mapping(
-    *,
-    tos_accepted: bool | None,
-    acceptance_timestamp: float,
-    **metadata,  # noqa: ANN003, ARG001
-) -> str:
-    """Map the acceptance status to a human-readable string."""
-    if tos_accepted is None:
-        return "not reviewed"
-    elif tos_accepted:
-        # convert timestamp to localized time
-        return (
-            datetime.utcfromtimestamp(acceptance_timestamp).astimezone().isoformat(" ")
-        )
-    else:
-        return "rejected"
 
 
 def execute(args: Namespace) -> int:
     """Execute the `tos` subcommand."""
     if args.accept:
         accept_tos(*context.channels)
-    elif args.reject:
-        reject_tos(*context.channels)
     elif args.view:
         view_tos(*context.channels)
-    else:
-        table = Table()
-        table.add_column("Channel")
-        table.add_column("Version")
-        table.add_column("Accepted")
-
-        for channel, metadata in get_tos(*context.channels):
-            table.add_row(
-                channel.base_url,
-                str(metadata["tos_version"]),
-                accepted_mapping(**metadata),
-            )
-
-        console = Console()
-        console.print(table)
     return 0
 
 
--- anaconda_conda_tos/tos.py
+++ anaconda_conda_tos/tos.py
@@ -10,17 +10,11 @@ from typing import TYPE_CHECKING
 from conda.models.channel import Channel
 
 from .exceptions import CondaToSMissingError
-from .metadata import (
-    get_all_tos_metadatas,
-    get_channel_tos_metadata,
-    write_metadata,
-)
+from .metadata import write_metadata
 from .remote import get_tos_metadata, get_tos_text
 
 if TYPE_CHECKING:
-    from typing import Iterable, Iterator
-
-    from .metadata import ToSMetaData
+    from typing import Iterable
 
 
 def get_channels(*channels: str | Channel) -> Iterable[Channel]:
@@ -57,26 +51,3 @@ def accept_tos(*channels: str | Channel) -> None:
             acceptance_timestamp=datetime.now(tz=timezone.utc),
             **metadata,
         )
-
-
-def reject_tos(*channels: str | Channel) -> None:
-    """Reject the ToS for the given channels."""
-    for channel in get_channels(*channels):
-        print(f"declining ToS for {channel}")
-        metadata = get_tos_metadata(channel)
-        write_metadata(channel, tos_accepted=False, **metadata)
-
-
-def get_tos(*channels: str | Channel) -> Iterator[tuple[Channel, ToSMetaData]]:
-    """List all channels and whether their ToS has been accepted."""
-    # list all active channels
-    seen: set[Channel] = set()
-    for channel in get_channels(*channels):
-        yield channel, get_channel_tos_metadata(channel)
-        seen.add(channel)
-
-    # list all other ToS that have been accepted
-    for channel, metadata in get_all_tos_metadatas():
-        if channel not in seen:
-            yield channel, metadata
-            seen.add(channel)
