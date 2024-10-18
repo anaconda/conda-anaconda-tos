9b3867c9c33754be194434e5bda3b0ae91013997
Ken Odegard <kodegard@anaconda.com>
2024-10-17 21:59:59 -0500

    Move remote functions into separate submodule

--- anaconda_conda_tos/tos.py
+++ anaconda_conda_tos/tos.py
@@ -6,65 +6,13 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from conda.base.context import context
-from conda.common.url import join_url
-from conda.gateways.connection.session import get_session
 from conda.models.channel import Channel
-from requests import HTTPError
 
 from .exceptions import CondaToSMissingError
+from .remote import get_tos_text
 
 if TYPE_CHECKING:
-    from typing import Final, Iterable, Literal
-
-    from requests import Response
-
-
-# remote endpoints
-TOS_TEXT: Final = "tos.txt"
-
-
-def get_tos_endpoint(
-    channel: str | Channel,
-    endpoint: Literal["tos.txt"],
-) -> Response:
-    """Get the ToS endpoint for the given channel."""
-    channel = Channel(channel)
-    if not channel.base_url:
-        raise ValueError(
-            "Channel must have a base URL. MultiChannel doesn't have endpoints."
-        )
-    if endpoint not in (TOS_TEXT,):
-        raise ValueError(f"Invalid ToS endpoint: {endpoint}")
-
-    session = get_session(channel.base_url)
-    endpoint = join_url(channel.base_url, TOS_TEXT)
-
-    saved_token_setting = context.add_anaconda_token
-    try:
-        # do not inject conda/binstar token into URL for two reasons:
-        # 1. ToS shouldn't be a protected endpoint
-        # 2. CondaHttpAuth.add_binstar_token adds subdir to the URL which ToS don't have
-        context.add_anaconda_token = False
-        response = session.get(
-            endpoint,
-            headers={"Content-Type": "text/plain"},
-            proxies=session.proxies,
-            auth=None,
-            timeout=(
-                context.remote_connect_timeout_secs,
-                context.remote_read_timeout_secs,
-            ),
-        )
-        response.raise_for_status()
-    except HTTPError as exc:
-        if exc.response.status_code == 404:
-            raise CondaToSMissingError(channel) from exc
-        else:
-            raise
-    finally:
-        context.add_anaconda_token = saved_token_setting
-    return response
+    from typing import Iterable
 
 
 def get_channels(*channels: str | Channel) -> Iterable[Channel]:
@@ -78,11 +26,6 @@ def get_channels(*channels: str | Channel) -> Iterable[Channel]:
             if channel not in seen:
                 yield channel
                 seen.add(channel)
-
-
-def get_tos_text(channel: str | Channel) -> str:
-    """Get the ToS full text for the given channel."""
-    return get_tos_endpoint(channel, TOS_TEXT).text.rstrip()
 
 
 def view_tos(*channels: str | Channel) -> None:
