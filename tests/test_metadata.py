6527c145c03920eec13e58f252ffa9553af87062
Ken Odegard <kodegard@anaconda.com>
2024-10-18 15:30:21 -0500

    Update test_metadata.py

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -17,9 +17,9 @@ from anaconda_conda_tos.remote import RemoteToSMetadata
 def test_write_metadata(tos_channel: str) -> None:
     channel = Channel(tos_channel)
     now = datetime.now(tz=timezone.utc).timestamp()
-    remote_metadata = RemoteToSMetadata(tos_version=42, **{uuid4().hex: uuid4().hex})
+    remote = RemoteToSMetadata(tos_version=42, **{uuid4().hex: uuid4().hex})
     metadata = ToSMetadata(
-        **remote_metadata.model_dump(),
+        **remote.model_dump(),
         tos_accepted=True,
         # the following fields are overridden in write_metadata
         acceptance_timestamp=now,
@@ -35,18 +35,15 @@ def test_write_metadata(tos_channel: str) -> None:
         write_metadata(tos_channel, "metadata")  # type: ignore[arg-type]
 
     with pytest.raises(ValidationError):
-        write_metadata(tos_channel, remote_metadata)
+        write_metadata(tos_channel, remote)
 
-    write_metadata(tos_channel, remote_metadata, tos_accepted=True)
-
-    with pytest.raises(ValidationError):
-        write_metadata(tos_channel, remote_metadata)
+    write_metadata(tos_channel, remote, tos_accepted=True)
 
     write_metadata(tos_channel, metadata)
     contents = get_tos_path(tos_channel, 42).read_text()
-    local_metadata = ToSMetadata.model_validate_json(contents)
-    assert local_metadata.model_fields == metadata.model_fields
+    local = ToSMetadata.model_validate_json(contents)
+    assert local.model_fields == metadata.model_fields
     assert all(
-        getattr(local_metadata, key) == getattr(metadata, key)
-        for key in set(local_metadata.model_fields) - {"acceptance_timestamp"}
+        getattr(local, key) == getattr(metadata, key)
+        for key in set(local.model_fields) - {"acceptance_timestamp"}
     )
