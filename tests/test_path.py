1972feefe1c85727dba85f53208c60ec9f140015
Ken Odegard <kodegard@anaconda.com>
2024-10-31 14:31:15 -0500

    Update test_path.py

--- tests/test_path.py
+++ tests/test_path.py
@@ -11,6 +11,8 @@ from conda.base.context import context
 from anaconda_conda_tos.path import (
     SYSTEM_TOS_ROOT,
     USER_TOS_ROOT,
+    get_all_channel_paths,
+    get_channel_paths,
     get_path,
     get_search_path,
     get_tos_dir,
@@ -31,13 +33,13 @@ def test_hash_channel(sample_channel: str, tos_channel: str) -> None:
         hash_channel("defaults")
 
 
-def test_get_tos_root(tmp_path: Path) -> None:
+def test_get_path(tmp_path: Path) -> None:
     assert get_path(SYSTEM_TOS_ROOT) == Path(context.conda_prefix, "conda-meta", "tos")
     assert get_path(USER_TOS_ROOT) == Path.home() / ".conda" / "tos"
     assert get_path(tmp_path) == tmp_path
 
 
-def test_get_tos_search_path(
+def test_get_search_path(
     monkeypatch: MonkeyPatch,
     tmp_path: Path,
     mock_tos_search_path: tuple[Path, Path],
@@ -57,3 +59,20 @@ def test_get_tos_path(tmp_path: Path, sample_channel: str) -> None:
         get_tos_path(tmp_path, sample_channel, 42)
         == get_tos_dir(tmp_path, sample_channel) / "42.json"
     )
+
+
+def test_get_all_channel_paths(tmp_path: Path) -> None:
+    (channel1 := tmp_path / "channel1").mkdir()
+    (json1 := channel1 / "1.json").touch()
+    (json2 := channel1 / "2.json").touch()
+    (channel2 := tmp_path / "channel2").mkdir()
+    (json3 := channel2 / "1.json").touch()
+    (json4 := channel2 / "2.json").touch()
+    assert sorted(get_all_channel_paths([tmp_path])) == [json1, json2, json3, json4]
+
+
+def test_get_channel_paths(tmp_path: Path, sample_channel: str) -> None:
+    (channel1 := tmp_path / hash_channel(sample_channel)).mkdir()
+    (json1 := channel1 / "1.json").touch()
+    (json2 := channel1 / "2.json").touch()
+    assert sorted(get_channel_paths(sample_channel, [tmp_path])) == [json1, json2]
