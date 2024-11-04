03f37d169635bf77e5b44a852a74c53177d1b031
Ken Odegard <kodegard@anaconda.com>
2024-11-04 11:24:21 -0600

    Fix test_remote.py

--- tests/test_remote.py
+++ tests/test_remote.py
@@ -2,18 +2,25 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from __future__ import annotations
 
+import os
 from typing import TYPE_CHECKING
 from uuid import uuid4
 
 import pytest
 
 from anaconda_conda_tos.exceptions import CondaToSInvalidError, CondaToSMissingError
-from anaconda_conda_tos.remote import get_endpoint, get_remote_metadata
+from anaconda_conda_tos.models import RemoteToSMetadata
+from anaconda_conda_tos.path import get_cache_path
+from anaconda_conda_tos.remote import (
+    get_cached_endpoint,
+    get_endpoint,
+    get_remote_metadata,
+    touch_cached_endpoint,
+    write_cached_endpoint,
+)
 
 if TYPE_CHECKING:
     from pytest_mock import MockerFixture
-
-    from anaconda_conda_tos.models import RemoteToSMetadata
 
 
 def test_get_endpoint(tos_channel: str, sample_channel: str) -> None:
@@ -32,7 +39,59 @@ def test_get_endpoint(tos_channel: str, sample_channel: str) -> None:
         get_endpoint(uuid4().hex)
 
 
-def test_get_tos_metadata(
+def test_get_cached_endpoint(sample_channel: str) -> None:
+    path = get_cache_path(sample_channel)
+    assert not path.exists()
+
+    # falsy cache_timeout
+    assert get_cached_endpoint(sample_channel, cache_timeout=0) is None
+    assert get_cached_endpoint(sample_channel, cache_timeout=0.0) is None
+    assert get_cached_endpoint(sample_channel, cache_timeout=None) is None
+    assert get_cached_endpoint(sample_channel, cache_timeout=False) is None
+
+    # invalid cache_timeout
+    with pytest.raises(TypeError):
+        get_cached_endpoint(sample_channel, cache_timeout=object())  # type: ignore[arg-type]
+
+    # missing file/no mtime
+    assert get_cached_endpoint(sample_channel) is None
+
+    # touch cache file in the past
+    path.touch()
+    os.utime(path, (path.stat().st_atime, path.stat().st_mtime - 100))
+
+    # cache file is fresh
+    assert get_cached_endpoint(sample_channel, cache_timeout=1000) == path
+
+    # cache file is stale
+    assert get_cached_endpoint(sample_channel, cache_timeout=10) is None
+
+
+def test_write_cached_endpoint(sample_channel: str) -> None:
+    remote_metadata = RemoteToSMetadata(
+        tos_version=42,
+        text=f"ToS full text\n\n{uuid4().hex}",
+        **{uuid4().hex: uuid4().hex},
+    )
+
+    path = get_cache_path(sample_channel)
+    assert not path.exists()
+    write_cached_endpoint(sample_channel, remote_metadata)
+    assert path.exists()
+    assert RemoteToSMetadata.model_validate_json(path.read_text()) == remote_metadata
+
+    with pytest.raises(TypeError):
+        write_cached_endpoint(sample_channel, object())  # type: ignore[arg-type]
+
+
+def test_touch_cached_endpoint(sample_channel: str) -> None:
+    path = get_cache_path(sample_channel)
+    assert not path.exists()
+    touch_cached_endpoint(sample_channel)
+    assert path.exists()
+
+
+def test_get_remote_metadata(
     tos_channel: str,
     sample_channel: str,
     tos_metadata: RemoteToSMetadata,
@@ -40,6 +99,7 @@ def test_get_tos_metadata(
 ) -> None:
     # get metadata of ToS channel
     assert get_remote_metadata(tos_channel) == tos_metadata
+    assert get_remote_metadata(tos_channel, cache_timeout=100) == tos_metadata
 
     # no metadata for sample channel
     with pytest.raises(CondaToSMissingError):
