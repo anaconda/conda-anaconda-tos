5b9b287f2e8fab74dfd8f5572b6a8bfc0790c184
Ken Odegard <kodegard@anaconda.com>
2024-12-05 09:22:29 -0600

    Apply suggestions from code review
    Co-authored-by: Jannis Leidel <jannis@leidel.info>

--- recipe/pre-unlink.bat
+++ recipe/pre-unlink.bat
@@ -1,1 +1,3 @@
-conda tos clean --all
+@echo off
+if "%CONDA_PREFIX%"=="" (set _PREFIX="%PREFIX%") else (set "_PREFIX=%CONDA_PREFIX%")
+"%_PREFIX%\python.exe" -m conda tos clean --all >>"%_PREFIX%\.messages.txt" 2>&1
