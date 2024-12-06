10c65422e6471d86ee1ee50ee4ef73c1538607dc
Ken Odegard <kodegard@anaconda.com>
2024-12-06 15:18:56 -0600

    Fix shell assignment

--- recipe/pre-unlink.sh
+++ recipe/pre-unlink.sh
@@ -2,5 +2,5 @@
 
 _PREFIX="${CONDA_PREFIX:-${PREFIX:-}}"
 _PYTHON="${PREFIX}/python.exe"
-[ -f "${_PYTHON}" ] || _PYTHON ="${_PREFIX}/bin/python"
+[ -f "${_PYTHON}" ] || _PYTHON="${_PREFIX}/bin/python"
 "${_PYTHON}" -m conda tos clean --all >>"${_PREFIX}/.messages.txt" 2>&1 || :
